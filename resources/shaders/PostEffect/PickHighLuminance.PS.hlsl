f0e9aaedb10a3bb455020d65283cd105f2279cfe
Taka <k022g0068@g.neec.ac.jp>
2024-12-12 00:55:50 +0900

    いらんもん削除 高光度抽出をパラメータとしてとれるように

--- resources/shaders/PostEffect/PickHighLuminance.PS.hlsl
+++ resources/shaders/PostEffect/PickHighLuminance.PS.hlsl
@@ -8,50 +8,24 @@ struct PixelShaderOutput
 };
 
 
-float32_t4 CheckHighLuminance(float32_t4 color)
+float32_t4 CheckHighLuminance(float32_t4 color,float32_t min,float32_t max)
 {
     float32_t3 pColor = float32_t3(0.2125f, 0.7154f, 0.0721f);
     float32_t grayScale = dot(color.rgb, pColor);
     
-    float32_t extract = smoothstep(0.6f, 0.9f, grayScale);
+    float32_t extract = smoothstep(min, max, grayScale);
     
     return color * extract;
-}
-
-float32_t4 ApplyGaussianBlur(float32_t2 uv)
-{
-    // 重み係数
-    float32_t weight[5] = { 0.227027f, 0.1945946f, 0.1216216f, 0.054054f, 0.016216f };
-    float32_t4 blurredColor = float32_t4(0, 0, 0, 0);
-
-    //画像サイズ
-    float32_t2 texelSize;
-    
-    texelSize.x = 1.0f/1280.0f;
-    texelSize.y = 1.0f/720.0f;
-    
-    // 水平方向のブラー
-    for (int i = -2; i <= 2; i++)
-    {
-        blurredColor += gTexture.Sample(gSampler, uv + float32_t2(texelSize.x * i, 0)) * weight[abs(i)];
-    }
-
-    // 垂直方向のブラー
-    float32_t4 finalBlur = float32_t4(0, 0, 0, 0);
-    for (int ii = -2; ii <= 2; ii++)
-    {
-        finalBlur += blurredColor * weight[abs(ii)] * gTexture.Sample(gSampler, uv + float32_t2(0, texelSize.y * ii));
-    }
-
-    return finalBlur;
 }
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
+    float32_t min=0.1f;
+    float32_t max=0.9f;
     // Step 1: 元の画像から高輝度部分を抽出
     float32_t4 color = gTexture.Sample(gSampler, input.texcoord);
-    float32_t4 highLuminanceColor = CheckHighLuminance(color);
+    float32_t4 highLuminanceColor = CheckHighLuminance(color,min,max);
 
     output.color = highLuminanceColor;
     return output;
